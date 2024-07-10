0be21815b525de12609d0776d65d5d654aa05dff
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-07-10 19:40:46 +0200

    initial commit

---
+++ houdini.config.js
@@ -0,0 +1,13 @@
+/// <references types="houdini-svelte">
+
+/** @type {import('houdini').ConfigFile} */
+const config = {
+	watchSchema: {
+		url: `env:BACKEND_URL`
+	},
+	plugins: {
+		'houdini-svelte': {}
+	}
+};
+
+export default config;
