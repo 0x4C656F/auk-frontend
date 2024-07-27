e5fc793928e2b7fc7ef60e5447006fd3ce180889
Lev Lavryniuk <levlavryniuk@outlook.com>
2024-07-27 12:08:10 +0200

    feat: Add console log to parsePathFromUrl function

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -57,5 +57,6 @@ export async function handleFetch({ event, request, fetch }) {
 function parsePathFromUrl(url: string) {
 	const [, ...path] = url.slice(7).split('/');
 	const parsedPath = '/' + path.join('/');
+	console.log('Parsed path:', parsedPath);
 	return parsedPath;
 }
